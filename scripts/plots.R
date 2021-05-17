432d9dc88dbd7d352fcbdbeb1d5380683095853b
Felipe Figueiredo <philsf79@gmail.com>
2021-05-17 16:08:09 -0300

    plots created

--- scripts/plots.R
+++ scripts/plots.R
@@ -4,19 +4,19 @@ library(survminer)
 
 # plots -------------------------------------------------------------------
 
-ggsurvplot(sf.1) +
+gg.overall <- ggsurvplot(sf.1, risk.table = TRUE) +
   ggtitle("Kaplan-Meier plot of prosthesis loosening") +
   xlab("Time (years)")
 
-ggsurvplot(sf.gender, pval = TRUE) +
+gg.gender <- ggsurvplot(sf.gender, pval = TRUE) +
   ggtitle("Kaplan-Meier plot of prosthesis loosening") +
   xlab("Time (years)")
 
-ggsurvplot(sf.smoker, pval = TRUE) +
+gg.smoker <- ggsurvplot(sf.smoker, pval = TRUE) +
   ggtitle("Kaplan-Meier plot of prosthesis loosening") +
   xlab("Time (years)")
 
-ggsurvplot(sf.joint, pval = TRUE) +
+gg.joint <- ggsurvplot(sf.joint, pval = TRUE) +
   ggtitle("Kaplan-Meier plot of prosthesis loosening") +
   xlab("Time (years)")
 
