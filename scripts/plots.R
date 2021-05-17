0cf6b4bff75b69b4a7d821c6e3d067703264ccda
Felipe Figueiredo <philsf79@gmail.com>
2021-05-17 14:47:47 -0300

    exploratory plots (not saved yet)

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,7 +1,23 @@
 # setup -------------------------------------------------------------------
 # library(ggplot2)
-# library(survminer)
+library(survminer)
 
 # plots -------------------------------------------------------------------
+
+ggsurvplot(sf.1) +
+  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
+  xlab("Time (years)")
+
+ggsurvplot(sf.gender, pval = TRUE) +
+  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
+  xlab("Time (years)")
+
+ggsurvplot(sf.smoker, pval = TRUE) +
+  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
+  xlab("Time (years)")
+
+ggsurvplot(sf.joint, pval = TRUE) +
+  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
+  xlab("Time (years)")
 
 
