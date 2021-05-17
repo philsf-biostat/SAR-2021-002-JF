d3de8e3fc7693d536772ef7e1f39978a33bd27ad
Felipe Figueiredo <philsf79@gmail.com>
2021-05-17 14:13:47 -0300

    table 2 prototype

--- scripts/inference.R
+++ scripts/inference.R
@@ -1,8 +1,26 @@
 # setup -------------------------------------------------------------------
 # library(tableone)
 # library(gt)
-# library(gtsummary)
+library(gtsummary)
+library(survival)
+library(survminer)
 
 # tables ------------------------------------------------------------------
 
+sf.1 <- survfit( Surv(time, event) ~ 1, analytic)
+sf.gender <- survfit( Surv(time, event) ~ gender, analytic)
+sf.joint <- survfit( Surv(time, event) ~ joint, analytic)
+sf.smoker <- survfit( Surv(time, event) ~ smoker, analytic)
 
+list_sf <- list(
+  sf.1,
+  sf.gender,
+  sf.smoker,
+  sf.joint
+)
+
+inf_tab <- tbl_survfit(list_sf, times = 2,
+            label_header = "2-year prosthesis survival"
+            ) %>% add_p() #%>% as_kable()
+
+inf_tab %>% as_kable()
