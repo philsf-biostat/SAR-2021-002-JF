0dad06b7f0168b9c766e8ff65dd075e6aa7d314b
Felipe Figueiredo <philsf79@gmail.com>
2021-05-17 16:27:01 -0300

    height/width standardized (same size as document defaults)

--- scripts/plots-save.R
+++ scripts/plots-save.R
@@ -1,20 +1,21 @@
 # setup -------------------------------------------------------------------
-
+width <- 6
+height <- 6
 
 # save plots --------------------------------------------------------------
 
 ggsave(filename = "figures/km_overall.png",
        plot = print(gg.overall),
-       width = 6, height = 6)
+       width = width, height = height)
 
 ggsave(filename = "figures/km_gender.png",
        plot = print(gg.gender),
-       width = 6, height = 6)
+       width = width, height = height)
 
 ggsave(filename = "figures/km_smoker.png",
        plot = print(gg.smoker),
-       width = 6, height = 6)
+       width = width, height = height)
 
 ggsave(filename = "figures/km_joint.png",
        plot = print(gg.joint),
-       width = 6, height = 6)
+       width = width, height = height)
