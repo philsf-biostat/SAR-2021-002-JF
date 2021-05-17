c1897c123f8eb53710df890b2bfdd1c71aa5e03b
Felipe Figueiredo <philsf79@gmail.com>
2021-05-17 16:25:17 -0300

    plots titles adjusted

--- scripts/plots.R
+++ scripts/plots.R
@@ -5,19 +5,16 @@ library(survminer)
 # plots -------------------------------------------------------------------
 
 gg.overall <- ggsurvplot(sf.1, risk.table = TRUE) +
-  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
+  # ggtitle("Prosthesis loosening") +
   xlab("Time (years)")
 
 gg.gender <- ggsurvplot(sf.gender, pval = TRUE) +
-  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
   xlab("Time (years)")
 
 gg.smoker <- ggsurvplot(sf.smoker, pval = TRUE) +
-  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
   xlab("Time (years)")
 
 gg.joint <- ggsurvplot(sf.joint, pval = TRUE) +
-  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
   xlab("Time (years)")
 
 
