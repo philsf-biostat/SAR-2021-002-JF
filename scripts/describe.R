ac3bb257c6b83e066afdaceb8c82c221387390c2
Felipe Figueiredo <philsf79@gmail.com>
2021-05-17 14:11:15 -0300

    descriptive table prototype (+ prototype 2)

--- scripts/describe.R
+++ scripts/describe.R
@@ -4,10 +4,25 @@
 # library(skimr) # skim
 # library(tableone)
 # library(gmodels) # CrossTable
+library(gtsummary)
 
 # exploratory -------------------------------------------------------------
 
 
 # tables ------------------------------------------------------------------
+
+desc_tab <- analytic %>%
+  select(gender, age, bmi, smoker, joint, status) %>%
+  tbl_summary(statistic = all_continuous() ~ "{mean} ({sd})") #%>% as_kable()
+
+desc_tab %>% as_kable()
+
+desc_tab2 <- analytic %>%
+  # group_by(status) %>%
+  select(gender, age, bmi, smoker, joint, status) %>%
+  tbl_summary(by = status, statistic = all_continuous() ~ "{mean} ({sd})") %>%
+  add_p(test = all_continuous() ~ "t.test") #%>% as_kable()
+
+desc_tab2 %>% as_kable()
 
 
