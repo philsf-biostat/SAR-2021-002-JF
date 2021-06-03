bd9354c5d2a71a383c3ffc681030338e4183848a
Felipe Figueiredo <philsf79@gmail.com>
2021-06-03 20:01:53 -0300

    plots removed from this branch - already in 6-plots branch

--- scripts/plots.R
+++ scripts/plots.R
@@ -4,20 +4,3 @@ library(survminer)
 
 # plots -------------------------------------------------------------------
 
-ggsurvplot(sf.1) +
-  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
-  xlab("Time (years)")
-
-ggsurvplot(sf.gender, pval = TRUE) +
-  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
-  xlab("Time (years)")
-
-ggsurvplot(sf.smoker, pval = TRUE) +
-  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
-  xlab("Time (years)")
-
-ggsurvplot(sf.joint, pval = TRUE) +
-  ggtitle("Kaplan-Meier plot of prosthesis loosening") +
-  xlab("Time (years)")
-
-
